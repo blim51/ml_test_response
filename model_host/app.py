299edf56d839625bb56fd68fa228ad3494f0aeb0
Benjamin Lim <benjaminclim1@yahoo.com>
2026-08-10 00:05:14 -0400

    added saving/loading model, endpoint sends back prediction on input array

--- model_host/app.py
+++ model_host/app.py
@@ -3,10 +3,14 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 from flask import Flask, request
+import joblib
+import numpy as np
+
+# load model on server launch:
+loaded_model = joblib.load("model.joblib")
 
 # get port
-ROOT_DIR = Path(__file__).resolve().parent.parent  # go up 2 levels, needs testing
-# print(ROOT_DIR)
+ROOT_DIR = Path(__file__).resolve().parent.parent
 ENV_PATH = ROOT_DIR / ".env"
 if ENV_PATH.exists():
     load_dotenv(dotenv_path=ENV_PATH)
@@ -26,7 +30,7 @@ def failure_response(message, code=404):
 
 # routes here
 
-@app.route("/query/")
+@app.route("/query/", methods=["POST"])
 def send_query():
     """
     Endpoint for getting result of questionaire from model
@@ -34,10 +38,18 @@ def send_query():
     body = json.loads(request.data)
     input_answers = body.get("answers")
     # check if array and is length 10
-    if 1 == 2: # place holder
+    if input_answers is None:
         return failure_response("missing input", 400)
-    return success_response({"hello": [2, 3]}, 201)
-    # send label back!
+    elif not isinstance(input_answers, list) or len(input_answers) != 10:
+        return failure_response("invalid input (not length 10 list)", 400)
+    else:
+        # send label back!
+        return success_response({"result": loaded_model.predict(np.array(input_answers).reshape(1, -1))[0]}, 201)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port = MODEL_PORT, debug = "true")
+
+# sent JSON via Postman
+# {
+#     "answers": [1, 1, 1, 2, 2, 2, 4, 3, 4, 4]
+# }
